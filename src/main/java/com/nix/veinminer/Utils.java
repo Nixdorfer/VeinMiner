a50649f832a53e0851364c2d721e4dc0b4072066
Nix <sunch_2k@iCloud.com>
2021-10-14 19:05:47 +0200

    Sync

---
+++ src/main/java/com/nix/veinminer/Utils.java
@@ -0,0 +1,7 @@
+package com.nix.veinminer;
+
+public class Utils {
+    static final String mod_name = "veinminer";
+    static final String mod_id = "Vein Miner";
+    static final String version = "0.9.0";
+}
---
+++ src/main/java/com/nix/veinminer/VeinMiner.java
@@ -0,0 +1,9 @@
+package com.nix.veinminer;
+
+import net.minecraftforge.fml.common.Mod;
+
+import static com.nix.veinminer.Utils.*;
+
+@Mod(mod_id)
+public class VeinMiner {
+}
